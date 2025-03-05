180fabe3980430c82d3d7b6088296fc9071e4a6a
bigbug314 <bigbug31415@gmail.com>
2025-03-05 08:57:48 +0100

    .

--- PiAI/src/main.rs
+++ PiAI/src/main.rs
@@ -3,5 +3,5 @@ mod neural_network;
 fn main() {
     let ascii_digits = b"3141592653589793238462643383279502884197169";
     let binary_digits: Vec<u8> = ascii_digits.iter().map(|c| c - b'0').collect();
-    println!("{}",binary_digits[0]);
+
 }
